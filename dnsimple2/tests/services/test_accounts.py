cc3e83cfac006f4df953aecbbc08ddc153b74f47
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2017-01-18 02:00:04 +0530

    Minor: Change name for consistency.

--- dnsimple2/tests/services/test_accounts.py
+++ dnsimple2/tests/services/test_accounts.py
@@ -2,10 +2,10 @@ from dnsimple2.resources import AccountResource
 from dnsimple2.tests.services.base import BaseServiceTestCase
 
 
-class AccountsServiceTests(BaseServiceTestCase):
+class AccountServiceTests(BaseServiceTestCase):
     @classmethod
     def setUpClass(cls):
-        super(AccountsServiceTests, cls).setUpClass()
+        super(AccountServiceTests, cls).setUpClass()
         cls.accounts = cls.client.accounts
 
     def test_get(self):
