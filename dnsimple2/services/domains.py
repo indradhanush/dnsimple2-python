1d7632aae6684159e8b3949a8995d698ca20d7fc
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2017-01-22 22:40:58 +0530

    Add support for fetching a specific domain.

--- dnsimple2/services/domains.py
+++ dnsimple2/services/domains.py
@@ -6,12 +6,20 @@ class DomainService(BaseService):
     def __init__(self, client):
         super(DomainService, self).__init__(client, '{account_id}/domains')
 
-    def get_url(self, account_id):
-        return self.url.format(account_id=account_id)
+    def get_url(self, account_id, domain=None):
+        url = self.url.format(account_id=account_id)
+        if domain is not None:
+            return url + '/{domain}'.format(domain=domain)
 
-    def get(self, account_id, **kwargs):
+        return url
+
+    def list(self, account_id, **kwargs):
         response = self.client.get(self.get_url(account_id), **kwargs)
         return [DomainResource(item) for item in response['data']]
+
+    def get(self, account_id, domain, **kwargs):
+        response = self.client.get(self.get_url(account_id, domain), **kwargs)
+        return DomainResource(response['data'])
 
     def create(self, account_id, data):
         response = self.client.post(self.get_url(account_id), data)
--- dnsimple2/tests/services/test_domains.py
+++ dnsimple2/tests/services/test_domains.py
@@ -10,21 +10,21 @@ class DomainServiceTests(BaseServiceTestCase):
     @classmethod
     def setUpClass(cls):
         super(DomainServiceTests, cls).setUpClass()
-        cls.domains = cls.client.domains
+        cls.domain = 'example-{uuid}.org'.format(uuid=uuid4().hex)
 
     @classmethod
     def tearDownClass(cls):
         # TODO: Delete domains created by tests
         pass
 
-    def test_get(self):
-        response = self.domains.get(424)
+    def test_list(self):
+        response = self.client.domains.list(424)
         for item in response:
             self.assertIsInstance(item, DomainResource)
 
     def test_create_with_no_data(self):
         with self.assertRaises(HTTPError) as e:
-            response = self.domains.create(424, data=None)
+            response = self.client.domains.create(424, data=None)
             self.assertIsNone(response)
 
         exception = e.exception
@@ -39,7 +39,7 @@ class DomainServiceTests(BaseServiceTestCase):
 
     def test_create_with_invalid_data(self):
         with self.assertRaises(HTTPError) as e:
-            response = self.domains.create(424, dict(name='invalid-domain'))
+            response = self.client.domains.create(424, dict(name='invalid-domain'))
             self.assertIsNone(response)
 
         exception = e.exception
@@ -53,6 +53,22 @@ class DomainServiceTests(BaseServiceTestCase):
         )
 
     def test_create_with_valid_data(self):
-        domain = 'example-{uuid}.org'.format(uuid=uuid4().hex)
-        response = self.domains.create(424, dict(name=domain))
+        response = self.client.domains.create(424, dict(name=self.domain))
         self.assertIsInstance(response, DomainResource)
+
+    def test_get_with_invalid_domain_name(self):
+        with self.assertRaises(HTTPError) as e:
+            response = self.client.domains.get(424, 'invalid-domain')
+            self.assertIsNone(response)
+
+        exception = e.exception
+        self.assertEqual(exception.response.status_code, 404)
+        self.assertEqual(exception.response.json(), {
+            "message": "Domain `invalid-domain` not found"
+        })
+
+    def test_get_with_valid_domain_name(self):
+        # test_create will execute before this, thus self.domain will be available
+        response = self.client.domains.get(424, self.domain)
+        self.assertIsInstance(response, DomainResource)
+        self.assertEqual(response.name, self.domain)
